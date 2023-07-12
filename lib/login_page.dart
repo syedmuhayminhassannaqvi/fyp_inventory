5e359191ffc173343b3e517d0ec8fe4a36b6883a
Muhammad Khan Khakwani <mohammad.khan5644@gmail.com>
2023-07-12 21:24:42 +0500

    ...

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -55,120 +55,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-        backgroundColor: Color(0xFFffffff),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Lottie.asset('animations/38435-register.json',
-                  height: 300, repeat: true, reverse: true, fit: BoxFit.cover),
-              Container(
-                height: 370,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
-                  ),
-                  color: Color(0xFFE8E8E8).withOpacity(0.5),
-                ),
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 40),
-                    child: Column(
-                      children: [
-                        Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              // Email/phone text form field,
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 10),
-                                child: TextFormField(
-                                  controller: _emailPhoneController,
-                                  decoration: InputDecoration(
-                                    hintText: 'Email or Phone Number',
-                                    errorText: _userError ? errorText : null,
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      borderSide:
-                                          BorderSide(color: Color(0xFF004096)),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      borderSide:
-                                          BorderSide(color: Color(0xFF004096)),
-                                    ),
-                                  ),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _userError = false;
-                                    });
-                                  },
-                                  validator: (value) {
-                                    if (value!.isEmpty) {
-                                      return 'Please enter your email or phone number';
-                                    } else if (!value.contains('@') &&
-                                        !RegExp(r'^(\+92)?[0-9]{10}$')
-                                            .hasMatch(value)) {
-                                      return 'Please enter a valid email or phone number';
-                                    }
-                                    return null;
-                                  },
-                                ),
-                              ),
-                              // Password text form field
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 10),
-                                child: TextFormField(
-                                  controller: _passwordController,
-                                  obscureText: _isPasswordObscured,
-                                  decoration: InputDecoration(
-                                    hintText: 'Password',
-                                    errorText:
-                                        _passwordError ? errorText : null,
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      borderSide:
-                                          BorderSide(color: Color(0xFF004096)),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      borderSide:
-                                          BorderSide(color: Color(0xFF004096)),
-                                    ),
-                                    suffixIcon: IconButton(
-                                      icon: Icon(
-                                        _isPasswordObscured
-                                            ? Icons.visibility
-                                            : Icons.visibility_off,
-                                        color: Colors.grey,
-                                      ),
-                                      onPressed: () {
-                                        setState(() {
-                                          _isPasswordObscured =
-                                              !_isPasswordObscured;
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _passwordError = false;
-                                    });
-                                  },
-                                  validator: (value) {
-                                    if (value!.isEmpty) {
-                                      return 'Please enter your password';
-                                    }
-                                    return null;
-                                  },
-                                ),
-                              ),
-                            ],
-=======
       backgroundColor: Color(0xFFffffff),
       body: SingleChildScrollView(
         child: Column(
@@ -274,8 +160,7 @@ class _LoginPageState extends State<LoginPage> {
                                 ),
                                 onPressed: () {
                                   setState(() {
-                                    _isPasswordObscured =
-                                    !_isPasswordObscured;
+                                    _isPasswordObscured = !_isPasswordObscured;
                                   });
                                 },
                               ),
@@ -291,7 +176,6 @@ class _LoginPageState extends State<LoginPage> {
                               }
                               return null;
                             },
->>>>>>> 52127dd101371c69da7aca0d99151d422f39ac6f
                           ),
                         ),
                         SizedBox(
@@ -335,14 +219,10 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
-<<<<<<< HEAD
-            ]));
-=======
             ),
           ],
         ),
       ),
     );
->>>>>>> 52127dd101371c69da7aca0d99151d422f39ac6f
   }
 }
